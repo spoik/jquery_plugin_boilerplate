8be94429f788d17edf4512dff0ccfb95cd2bf14f
Spike <sfrye@periscope.com>
2013-04-19 10:22:23 -0500

    Adding comments and defualt option merging

--- jquery_boilerplate.js
+++ jquery_boilerplate.js
@@ -4,6 +4,8 @@
 		init : function( options ) {
 			return this.each(function(){
 				var $this = $(this),
+
+					// Change myPlugin to the name of your plugin
 					data = $this.data('myPlugin');
 
 				// If the plugin hasn't been initialized yet
@@ -12,9 +14,13 @@
 						Do more setup stuff here
 					*/
 
-					$(this).data('myPlugin', {
-						target: $this,
-					});
+					// Merge default settings with user settings
+					var settings = $.extend(true, {
+						setting: 'setting value'
+					}, options);
+
+					// Change myPlugin to the name of your plugin
+					$(this).data('myPlugin', settings);
 				}
 			});
 		},
@@ -23,22 +29,26 @@
 		destroy : function( ) {
 			return this.each(function(){
 
-				var $this = $(this),
-					data = $this.data('myPlugin');
+				var $this = $(this);
 
+				// Change myPlugin to the name of your plugin
+
+				// Remove any events bound to elements for this plugin
 				// $(element(s)).unbind('.myPlugin');
 				$this.removeData('myPlugin');
 
 			});
-		},
+		}
 	};
 
+	// Change myPlugin to the name of your plugin
 	$.fn.myPlugin = function( method ) {
 		if ( methods[method] ) {
 			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
 		} else if ( typeof method === 'object' || ! method ) {
 			return methods.init.apply( this, arguments );
 		} else {
+			// Change myPlugin to the name of your plugin
 			$.error( 'Method ' +  method + ' does not exist on jQuery.myPlugin' );
 		}
 	};
