8daf141f9f67a878a2c3e0aa49797ef0d2beea59
Spike <sfrye@periscope.com>
2013-04-19 10:16:20 -0500

    Updated name of boilerplate plugin

--- jquery_boilerplate.js
+++ jquery_boilerplate.js
@@ -4,7 +4,7 @@
 		init : function( options ) {
 			return this.each(function(){
 				var $this = $(this),
-					data = $this.data('gridViewNav');
+					data = $this.data('myPlugin');
 
 				// If the plugin hasn't been initialized yet
 				if ( ! data ) {
@@ -12,7 +12,7 @@
 						Do more setup stuff here
 					*/
 
-					$(this).data('gridViewNav', {
+					$(this).data('myPlugin', {
 						target: $this,
 					});
 				}
@@ -24,22 +24,22 @@
 			return this.each(function(){
 
 				var $this = $(this),
-					data = $this.data('gridViewNav');
+					data = $this.data('myPlugin');
 
-				// $(element(s)).unbind('.gridViewNav');
-				$this.removeData('gridViewNav');
+				// $(element(s)).unbind('.myPlugin');
+				$this.removeData('myPlugin');
 
 			});
 		},
 	};
 
-	$.fn.gridViewNav = function( method ) {
+	$.fn.myPlugin = function( method ) {
 		if ( methods[method] ) {
 			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
 		} else if ( typeof method === 'object' || ! method ) {
 			return methods.init.apply( this, arguments );
 		} else {
-			$.error( 'Method ' +  method + ' does not exist on jQuery.gridViewNav' );
+			$.error( 'Method ' +  method + ' does not exist on jQuery.myPlugin' );
 		}
 	};
 })( jQuery );
